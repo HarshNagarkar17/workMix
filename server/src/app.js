cd81c3b611111e949a8bb8be4b3b986e7afe7fef
Harsh Nagarkar <harshvn187@gmail.com>
2024-04-23 21:01:57 +0530

    initial

---
+++ server/src/app.js
@@ -0,0 +1,23 @@
+import express from  "express"
+import cors from "cors";
+import helmet from "helmet";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors({origin:[process.env.CLIENT_URL]}));
+app.use(helmet());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.get("/", (req,res) => {
+    res.send("/ page");
+})
+
+app.listen(port, () => {
+    console.log(`server started on http://localhost:${port}`)
+})
